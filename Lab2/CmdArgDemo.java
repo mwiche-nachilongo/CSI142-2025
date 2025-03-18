8f51ccad0c7b4655e1c071789b95b36ecbd7c4e3
mwiche-nachilongo <dinachilongo@gmail.com>
2025-03-18 13:55:33 +0200

    Update CmdArgDemo.java

--- Lab2/CmdArgDemo.java
+++ Lab2/CmdArgDemo.java
@@ -2,14 +2,12 @@
 * @author: Mwiche Dina Nachilongo 202208650.
 @version: 1.0
 Description: Program to demostrate command line arguments without prompting the user.
-To compile:javac CmdArgDemo Hello
-To execute: java CmdArgDemo
+To compile:javac CmdArgDemo.java
+To execute: java CmdArgDemo Hello
  */
 
 public class CmdArgDemo {
     public static void main (String []args ) {
-    Scanner in = new Scanner (System.in);
-    
     if (args.legnth == 0) {
         System.out.println("No arguments provided");
         }
