ba8c2f8c62d900ff3c7a313b1dc1b2ead1a85c49
mwiche-nachilongo <dinachilongo@gmail.com>
2025-03-18 11:06:08 +0200

    Update DoWhileSentinel.java

--- Lab2/DoWhileSentinel.java
+++ Lab2/DoWhileSentinel.java
@@ -1,18 +1,28 @@
+/**Filename: DoWhileSentinel
+ * @author: Mwiche Nachilongo
+ * @version: 1.0
+ * Description: program to implement do-while (Expense tracker)
+ * To compile:javac DoWhileSentnel.java
+ * To execute: java DoWhileSentinel
+*/
+
 import java.util.Scanner;
-public class DoWhileSentinel{
-    public static void main (String [] args){
-        Scanner in = new Scanner (System.in);
+
+public class DoWhileSentinel {
+    public static void main (String [] args) {
         
+        Scanner in = new Scanner (System.in);
         double expense;
         int count = 0;
-        int sentinel = -1;
         double total = 0;
          
         do{ 
-            System.out.println("Enter expense:");
+            System.out.println("Enter expense:"); // To prompt user
            expense = in.nextDouble();
            count++;
-        total += expense;
+        total = expense + total ;
         }while (expense > 0);
+
+        System.out.println("The total of the expenses entered is" + expense);
     }
 }
